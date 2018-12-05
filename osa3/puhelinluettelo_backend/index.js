2569d36662b38c86b273aba202ddf824297fa62f
Harri Mehtälä <harri.mehtala@helsinki.fi>
2018-12-05 16:25:54 +0200

    3.4 ja 3.5

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -50,34 +50,35 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-// const generateId = () => {
-//     const maxId = persons.length > 0 ? persons.map(p => p.id)
-//         .sort((a, b) => a - b).reverse()[0] : 1
-//     return maxId + 1
-// }
+const generateId = () => {
+    // const maxId = persons.length > 0 ? persons.map(p => p.id)
+    //     .sort((a, b) => a - b).reverse()[0] : 1
+    // return maxId + 1
+    return Math.floor(Math.random() * 10000) + 1
+}
 
-// app.post('/api/persons', (request, response) => {
-//     const body = request.body
-//     if (body.name === undefined) {
-//         return response.status(400).json({ error: 'name missing' })
-//     }
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (body.name === undefined) {
+        return response.status(400).json({ error: 'name missing' })
+    }
 
-//     const person = {
-//         name: body.content,
-//         number: body.number,
-//         id: generateId()
-//     }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
 
-//     persons = persons.concat(person)
-//     response.json(person)
-// })
+    persons = persons.concat(person)
+    response.json(person)
+})
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     notes = persons.filter(pers => pers.id !== id)
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(pers => pers.id !== id)
 
-//     response.status(204).end()
-// })
+    response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
