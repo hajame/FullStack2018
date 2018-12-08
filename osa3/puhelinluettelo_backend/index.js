a6193809615ef0c3961d911c4082406f6369e6ea
Harri Mehtälä <harri.mehtala@helsinki.fi>
2018-12-08 12:25:34 +0200

    teht 3.8

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -4,7 +4,17 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      JSON.stringify(req.body), '-',
+      tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+  })
+)
 
 let persons = [
     {
