ce79614d69703c543c6428760253be9cd58c6e60
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-03-07 18:51:26 +0200

    6.9 (HY2019) refactoring

--- osa6/redux-anecdotes/src/index.js
+++ osa6/redux-anecdotes/src/index.js
@@ -2,20 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import App from './App'
-import anecdoteReducer, { createAnecdote } from './reducers/anecdoteReducer'
-import notificationReducer, { setNotification } from './reducers/notificationReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
   notification: notificationReducer
 })
-
 const store = createStore(reducer)
-
-// THE FOLLOWING WORKS, TRY IT!
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(createAnecdote('this is a new anecdote'))
-// store.dispatch(setNotification('your new notification!!'))
 
 const render = () => {
   ReactDOM.render(
