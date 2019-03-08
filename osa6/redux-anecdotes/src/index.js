ffe3d94a9231e8437bb3ed95a89a7344e57f0c0c
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-03-08 14:21:50 +0200

    6.16 (HY2019) anecdotes are fetched from db

--- osa6/redux-anecdotes/src/index.js
+++ osa6/redux-anecdotes/src/index.js
@@ -7,13 +7,13 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 
+
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
   notification: notificationReducer,
   filter: filterReducer
 })
 const store = createStore(reducer)
-
 
 ReactDOM.render(
   <Provider store={store}>
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
 export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
@@ -27,17 +16,14 @@ export const voteFor = (id) => {
   }
 }
 
-const asObject = (anecdote) => {
+export const initializeAnecdotes = (anecdotes) => {
   return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   // console.log('anecdoteReducer: state, action: ', state, action)
   switch (action.type) {
     case 'VOTE':
@@ -54,6 +40,8 @@ const reducer = (state = initialState, action) => {
       return state
         .concat(action.data)
         .sort((a, b) => b.votes-a.votes)
+    case 'INIT_ANECDOTES':
+      return action.data
     default:
       return state
   }
